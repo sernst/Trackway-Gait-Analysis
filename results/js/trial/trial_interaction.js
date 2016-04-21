e8f79fa4e7ec5c0e9f911c32df7780c7fcd2aba0
sernst <swernst@gmail.com>
2016-04-20 23:57:14 -0500

    Trial SVG snapshots can now be saved from within trial reports

--- results/js/trial/trial_interaction.js
+++ results/js/trial/trial_interaction.js
@@ -4,6 +4,36 @@
     var exports = window.SIM || {};
     window.SIM = exports;
 
+
+    function saveSvg() {
+        var wasPaused = exports.animation.paused;
+        var frame = exports.DATA.frames[exports.animation.frameIndex];
+
+        exports.animation.paused = true;
+
+        try {
+            var isFileSaverSupported = !!new Blob();
+        } catch (e) {
+            alert("Save not supported on this browser");
+        }
+
+        var cycle = Math.floor(frame.time);
+        var phase = Math.round(100.0 * (frame.time - cycle));
+
+        var time = 'C' + cycle + 'p' + phase;
+        var title = $('title').html().replace(/\s/g, '-') + '_' + time;
+
+        var html = d3.select($('.svg-box svg')[0])
+            .attr("title", title)
+            .node().parentNode.innerHTML;
+
+        var blob = new Blob([html], {type: "image/svg+xml"});
+        window.saveAs(blob, title + ".svg");
+
+        exports.animation.paused = wasPaused;    
+    }
+    exports.saveSvg = saveSvg;
+
     /**
      * A callback executed by clicking on one of the playback control buttons
      * in the animation header section of the trial page. The action taken
@@ -17,6 +47,9 @@
         var frameAdjust = 0;
 
         switch (role) {
+            case 'save-svg':
+                return exports.saveSvg();
+
             case 'toggle-play':
                 exports.animation.paused = !exports.animation.paused;
                 $('.play-icon').toggle(exports.animation.paused);
